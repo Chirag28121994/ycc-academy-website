37c5719dca43e9206ad0bf2520d78f2eb5f49b81
chirag_bhadoria <chirag_bhadoria@epam.com>
2026-05-18 12:57:50 +0530

    update BaseLayout

--- nuclear-nova/astro.config.mjs
+++ nuclear-nova/astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://chirag28121994.github.io',
+  base: '/ycc-academy-website',
   vite: {
     plugins: [tailwindcss()]
   }
