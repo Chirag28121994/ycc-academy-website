0d65a4c210faf3ce04c3d7ce05f509fcd47ae32d
chirag_bhadoria <chirag_bhadoria@epam.com>
2026-05-20 16:14:55 +0530

    add location & google form

--- nuclear-nova/src/config.ts
+++ nuclear-nova/src/config.ts
@@ -6,7 +6,9 @@ export const SITE = {
   admissionStatus: "Admission Open 2026-27",
   address: "Sabzi Mandi ke samne, Karnal ki Bagiya, Pinto Park, Gwalior (M.P.)",
   locationShort: "Pinto Park, Gwalior",
-  googleFormUrl: "https://docs.google.com/forms/d/e/1FAIpQLSccVf3X9U_placeholder/viewform?embedded=true",
+  googleFormUrl: "https://forms.gle/jaB6QbQrJQvXMdWw9",
+  googleMapsShareUrl: "https://maps.app.goo.gl/FePvkofhWDqsBieZ7",
+  googleMapsEmbedUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3602.0!2d78.1828!3d26.2183!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zWUNDIEFjYWRlbXkgUGludG8gUGFyayBHd2FsaW9y!5e0!3m2!1sen!2sin!4v1716000000000!5m2!1sen!2sin",
   year: new Date().getFullYear(),
 };
 
